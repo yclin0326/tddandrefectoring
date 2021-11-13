6845278e10fcd5b52fe42b973c1efe5afe9bfd23
Andy Lin <yclin0326@gmail.com>
2021-11-13 16:30:29 +0800

    init

---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Budget
+    {
+        public string YearMonth { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class BudgetService
+    {
+        private IBudgetRepo _repo;
+        public BudgetService(IBudgetRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public decimal Query(DateTime start, DateTime end)
+        {
+            return 0;
+        }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+}
